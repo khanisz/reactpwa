faef5a7fb9ab561e39515b5360aebd89c9d71aae
Krzysztof Hanisz <krzysztof.hanisz@gmail.com>
2019-01-14 23:31:46 +0100

    Text component changed to Card

--- src/TextComponent.js
+++ src/TextComponent.js
@@ -1,39 +1,61 @@
-import { Typography } from "@material-ui/core";
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
-class TextComponent extends Component {
-  render() {
-    const styles = {
-      root: {
-        width: "100%",
-        maxWidth: 500,
-        padding: 20
-      }
-    };
-
-    return (
-      <div style={{ padding: 15 }}>
-        <div style={{ margin: 10 }}>
-          <Typography variant="h4" align="center">
-            {this.props.text}
-          </Typography>
-        </div>
-        <div>
-          <Typography variant="body1" paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas et
-            felis ultrices, dictum massa in, rutrum justo. Mauris ac enim sed
-            purus egestas sodales. Sed imperdiet dolor at laoreet sollicitudin.
-            Etiam et mi sed lorem rhoncus varius ac sed arcu. Suspendisse
-            potenti. Nullam sed mauris justo. Aliquam fermentum justo risus. Sed
-            molestie ex quis tellus cursus, nec auctor purus bibendum. Nam
-            pretium iaculis semper. Nullam scelerisque vulputate justo, a
-            tincidunt quam mollis non. Proin ornare volutpat lorem sit amet
-            lobortis.
-          </Typography>
-        </div>
-      </div>
-    );
+const styles = {
+  card: {
+    minWidth: 275,
+    margin: 10
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
   }
+};
+
+function TextComponent(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
+  const { text } = props;
+
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        />
+        <Typography variant="h5" component="h2">
+          {text}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary" />
+        <Typography component="p">
+          Breslau, 1938 rok. Przenieście się w czasie i poznajcie największą
+          tajemnicę przedwojennego Wrocławia. Znika kobieta podejrzana o zdradę
+          oraz śledzący ją na zlecenie męża detektywi. Teraz wszystko w Waszych
+          rękach. Zanim jednak odnajdziecie trop, najpierw musicie odnaleźć
+          siebie nawzajem w podziemiach Starego Miasta...
+        </Typography>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default TextComponent;
+TextComponent.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(TextComponent);
