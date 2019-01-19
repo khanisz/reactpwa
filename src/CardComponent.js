5f81e9ea7d1e8cee5016f3fce6d485a8db657f54
Krzysztof Hanisz <krzysztof.hanisz@gmail.com>
2019-01-19 17:59:32 +0100

    +Card visibility

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -3,29 +3,39 @@ import TextComponent from "./TextComponent";
 import QuestionComponent from "./QuestionComponent";
 import ImageComponent from "./ImageComponent";
 
-
 class CardComponent extends Component {
-    constructor(props) {
-        super(props);
-        console.log(props);
-        
-    }
-    render() {
-      const { component } = this.props;
-      let card;
-      if (component.type == 'textC'){
-        card = <TextComponent title={component.title} text={component.text}></TextComponent>
-      } else if (component.type == 'imageC'){
-        card = <ImageComponent title={component.title} text={component.text} url={component.url}></ImageComponent>
-      } else if (component.type == 'questionC'){
-        card = <QuestionComponent title={component.title} question={component.question} answer={component.answer} resolution={component.resolution} ></QuestionComponent>
-      } 
-      return (
-        <div>
-            {card}
-        </div>
-      );
-    }
+  constructor(props) {
+    super(props);
+    console.log(props);
   }
-  
-  export default CardComponent;
+  render() {
+    const { component } = this.props;
+    let card;
+    if ((component.visible == true)) {
+      if (component.type == "textC") {
+        card = <TextComponent title={component.title} text={component.text} />;
+      } else if (component.type == "imageC") {
+        card = (
+          <ImageComponent
+            title={component.title}
+            text={component.text}
+            url={component.url}
+          />
+        );
+      } else if (component.type == "questionC") {
+        card = (
+          <QuestionComponent
+            title={component.title}
+            question={component.question}
+            answer={component.answer}
+            resolution={component.resolution}
+          />
+        );
+      }
+    }
+
+    return <div>{card}</div>;
+  }
+}
+
+export default CardComponent;
