0588c63f70eb0a2598701110ed29eb23fd3d6d07
Krzysztof Hanisz <krzysztof.hanisz@gmail.com>
2019-01-19 19:35:45 +0100

    Map update

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import GoogleMapReact from "google-map-react";
 
-import MyGreatPlace from './my_great_place.jsx';
+import MyGreatPlace from "./my_great_place.jsx";
 
 class MapComponent extends Component {
   static defaultProps = {
     center: {
       lat: 51.11,
-      lng: 17.02
+      lng: 17.0
     },
     zoom: 14
   };
@@ -20,10 +20,9 @@ class MapComponent extends Component {
           defaultCenter={this.props.center}
           defaultZoom={this.props.zoom}
         >
-        
-          <MyGreatPlace lat={51.109306} lng={17.030001} text={'A'}/>
-          <MyGreatPlace lat={51.107756} lng={17.027516} text={'B'}/>
-          <MyGreatPlace lat={51.111528} lng={17.038073} text={'C'}/>
+          <MyGreatPlace lat={51.109306} lng={17.030001} text={"1"} />
+          <MyGreatPlace lat={51.107756} lng={17.027516} text={"2"} />
+          <MyGreatPlace lat={51.111528} lng={17.038073} text={"3"} />
         </GoogleMapReact>
       </div>
     );
